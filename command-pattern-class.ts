fe116516bdc7eea8fd973a6c2df2ce2138be4ec3
rakibrahman <md.rr.talukder@gmail.com>
2024-04-21 16:39:46 +0600

    command pattern with class

---
+++ command-pattern-class.ts
@@ -0,0 +1,89 @@
+abstract class Command<State> {
+    abstract execute(state: State): State;
+    abstract undo(state: State): State;
+
+}
+
+
+
+class CommandStack<State> {
+    private stack: Command<State>[] = []
+    constructor(private _state: State) {
+
+    }
+
+
+    get state() {
+        return this._state;
+    }
+
+    execute(command: Command<State>) {
+        this._state = command.execute(this._state);
+        this.stack.push(command);
+
+    }
+
+    undo() {
+        const command = this.stack.pop();
+        if (command) {
+            this._state = command.undo(this._state);
+        }
+    }
+
+    setState(values: State) {
+        this._state = values
+    }
+}
+
+
+
+class AddOne extends Command<number> {
+    execute(state: number): number {
+        return state + 1
+    }
+    undo(state: number): number {
+        return state - 1
+    }
+}
+
+const commandStack = new CommandStack<number>(0);
+console.log(commandStack.state)
+
+commandStack.execute(new AddOne())
+
+console.log(commandStack.state)
+commandStack.undo()
+console.log(commandStack.state)
+
+
+class ManageNames extends Command<string[]> {
+
+    constructor(private value: string) {
+        super();
+    }
+
+
+    execute(state: string[]): string[] {
+        state.push(this.value)
+        return state
+    }
+    undo(state: string[]): string[] {
+        state.pop()
+        return state
+    }
+}
+
+const cs = new CommandStack<string[]>([])
+cs.setState(['123','rakib'])
+console.log(cs.state)
+// cs.execute(new ManageNames('rakib'))
+// cs.execute(new ManageNames('pain'))
+cs.execute(new ManageNames('minato'))
+console.log(cs.state)
+
+// cs.undo()
+cs.undo()
+
+
+
+console.log(cs.state)
